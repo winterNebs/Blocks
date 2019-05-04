27580f6f2fdec9c3cadbee34e720bc1d81029689
winterNebs <isaac@wen.ca>
2019-05-04 17:48:55 -0400

    - Made a few structural changes: - Using CommonJS instead of AMD - Removed namespaces

---
+++ Blocks/Gruntfile.js
@@ -0,0 +1,72 @@
+﻿module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        //concat: {
+        //    dist: {
+        //        src: ['./dist/src/**/*.js'],
+        //        dest: './dist/bundle.js',
+        //        options: {
+        //            banner: ";(function( window, undefined ){ \n 'use strict';",
+        //            footer: "}( window ));"
+        //        }
+        //    }
+        //},
+        browserify: {
+            dist: {
+                files: {
+                    './dist/bundled.js': ['./dist/src/**/*.js']
+                },
+                options: {
+                    plugin: [
+                        [
+                            'remapify', [{
+                                src: './dist/src/**/*.js',  // glob for the files to remap
+                                expose: 'asc', // this will expose `__dirname + /client/views/home.js` as `views/home.js`
+                                cwd: __dirname  // defaults to process.cwd()
+                            }
+                            ]
+                        ]
+                    ],
+                    browserifyOptions: {
+                        debug: true,
+                        standalone: 'asc'
+                    },
+                }
+            }
+        },
+        //uglify: {
+        //    options: {
+        //        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        //    },
+        //    build: {
+        //        src: 'src/<%= pkg.name %>.js',
+        //        dest: 'build/<%= pkg.name %>.min.js'
+        //    }
+        //}
+    });
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-browserify');
+
+    // Load the plugin that provides the "uglify" task.
+    //grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['browserify']);
+   // grunt.registerTask('default', ['concat', 'browserify'])
+    //grunt.registerTask('default', function () {
+    //    var browserify = require('browserify');
+    //    var fs = require('file-system');
+    //    var done = this.async();
+    //
+    //    browserify({ debug: true })
+    //        .add('./dist/**/*.js')
+    //        .bundle()
+    //        .on('error', function (err) {
+    //            console.log(err);
+    //        })
+    //        .pipe(fs.createWriteStream('./dist/bundled.js')
+    //            .on('end', done));});
+    //grunt.registerTask('default', ['uglify']);
+};
---
+++ Blocks/require.js
@@ -0,0 +1,7 @@
+﻿// Set up any config you need (you might not need this)
+require.config({
+  basePath: "/src"
+});
+
+// Tell RequireJS to load your main module (and its dependencies)
+require("main");
